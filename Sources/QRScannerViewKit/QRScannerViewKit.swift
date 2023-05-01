e4b120e0902dfd25b291b9e77f962577002bf9d9
robin george <robin.g@hubspire.com>
2023-05-02 01:14:32 +0530

    QRScanner view code is completed in the generic form. (initial Commit)

--- Sources/QRScannerViewKit/QRScannerViewKit.swift
+++ Sources/QRScannerViewKit/QRScannerViewKit.swift
@@ -1,6 +1,141 @@
-public struct QRScannerViewKit {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import AVFoundation
+import SwiftUI
+import UIKit
 
-    public init() {
+@available (iOS 16.0, *)
+public struct QRCodeScannerView: UIViewControllerRepresentable {
+    // The handler that will be called with the scanned code
+    public var codeHandler: (String) -> Void
+    
+    // Opacity of the overlay
+    public var opacity: Double
+    
+    // Height of the frame
+    public var frameHeight: CGFloat
+    
+    public init(opacity: Double = 0.5, frameHeight: CGFloat = 250, codeHandler: @escaping (String) -> Void) {
+        self.opacity = opacity
+        self.frameHeight = frameHeight
+        self.codeHandler = codeHandler
+    }
+    
+    public func makeUIViewController(context: Context) -> ScannerViewController {
+        let viewController = ScannerViewController()
+        viewController.codeHandler = codeHandler
+        return viewController
+    }
+    
+    public func updateUIViewController(_ uiViewController: ScannerViewController, context: Context) {
+    }
+}
+
+@available (iOS 16.0, *)
+public class ScannerViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate, ObservableObject {
+    public var codeHandler: (String) -> Void = { _ in }
+    public var captureSession: AVCaptureSession!
+    public var previewLayer: AVCaptureVideoPreviewLayer!
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        captureSession = AVCaptureSession()
+        
+        guard let videoCaptureDevice = AVCaptureDevice.default(for: .video) else { return }
+        let videoInput: AVCaptureDeviceInput
+        
+        do {
+            videoInput = try AVCaptureDeviceInput(device: videoCaptureDevice)
+        } catch {
+            return
+        }
+        
+        if (captureSession.canAddInput(videoInput)) {
+            captureSession.addInput(videoInput)
+        } else {
+            failed()
+            return
+        }
+        
+        let metadataOutput = AVCaptureMetadataOutput()
+        
+        if (captureSession.canAddOutput(metadataOutput)) {
+            captureSession.addOutput(metadataOutput)
+            
+            metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+            metadataOutput.metadataObjectTypes = [.qr]
+        } else {
+            failed()
+            return
+        }
+        
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+       
+        DispatchQueue.global(qos: .background).async {
+ 
+            // start running captureSession
+            self.captureSession.startRunning()
+        }
+    }
+    
+    public func failed() {
+        let ac = UIAlertController(title: "Scanning not supported", message: "Your device does not support scanning a code from an item. Please use a device with a camera.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+        captureSession = nil
+    }
+    
+    public func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        captureSession.stopRunning()
+        
+        if let metadataObject = metadataObjects.first {
+            guard let readableObject = metadataObject as? AVMetadataMachineReadableCodeObject else { return }
+            guard let stringValue = readableObject.stringValue else { return }
+            AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+            found(code: stringValue)
+        }
+        dismiss(animated: true)
+    }
+    
+    public func found(code: String) {
+        codeHandler(code)
+    }
+}
+
+@available (iOS 16.0, *)
+public struct QRScannerViewBorderDisplay: View {
+    public init() {}
+    
+    public var body: some View {
+        Group {
+            HStack {
+                VStack {
+                    Color.white
+                }
+                .frame(width: 10, height: 100)
+                Spacer()
+                VStack {
+                    Color.white
+                }
+                .frame(width: 10, height: 100)
+            }
+            .frame(width: 250, height: 100)
+            
+            VStack {
+                HStack {
+                    Color.white
+                }
+                .frame(width: 100, height: 10)
+                Spacer()
+                HStack {
+                    Color.white
+                }
+                .frame(width: 100, height: 10)
+            }
+            .frame(width: 250, height: 250)
+        }
     }
 }

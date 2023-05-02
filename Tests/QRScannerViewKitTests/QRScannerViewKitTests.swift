39252a3c2653dda7d93ee45f717727f7657b9e19
robin george <robin.g@hubspire.com>
2023-05-02 11:23:15 +0530

    git commit -m "removed test case - version 1.2.3"

--- Tests/QRScannerViewKitTests/QRScannerViewKitTests.swift
+++ Tests/QRScannerViewKitTests/QRScannerViewKitTests.swift
@@ -1,11 +1,2 @@
 import XCTest
 @testable import QRScannerViewKit
-
-final class QRScannerViewKitTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(QRScannerViewKit().text, "Hello, World!")
-    }
-}

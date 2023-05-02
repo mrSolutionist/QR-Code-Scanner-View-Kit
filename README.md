71858439de586cfb552723ac8649492ca7d4507a
robin george <robin.g@hubspire.com>
2023-05-02 13:38:53 +0530

    Documentation added and few changes in the border display

--- README.md
+++ README.md
@@ -9,3 +9,72 @@ The QR code scanner view can be easily integrated into your iOS app using the pr
 Created for SwiftUI, 
 by Robin Geroge
 iOS Developer 
+
+
+# QRScannerViewKit Package Documentation
+The QRScannerViewKit package provides a SwiftUI view that can be used to scan QR codes using the device's camera. It creates a full-screen camera view with a square portion that acts as the camera, and scans for QR codes within that square. When a code is detected, a handler function that you provide is called with the scanned code as a parameter.
+
+# Installation
+The QRScannerViewKit package can be installed using Swift Package Manager. To install it in your Xcode project, follow these steps:
+
+In Xcode, open your project.
+Click on File > Swift Packages > Add Package Dependency.
+In the "Choose Package Repository" dialog, enter the URL of the QRScannerView package: https://github.com/mrSolutionist/QRScannerViewKit 
+Click on Next, and select the version of the package that you want to use.
+Click on Next again, and select the target where you want to add the package.
+Click on Finish.
+
+
+#Usage
+To use the QRScannerView in your SwiftUI view, follow these steps:
+
+Import the QRScannerViewKit package at the top of your file:
+
+```
+import QRScannerViewKit
+```
+
+or 
+
+```
+import QRScannerView
+```
+To use the QRScannerView, it is recommended to present it as a full-screen modal sheet or cover. This is because the view is designed to automatically dismiss itself once it successfully captures a QR code.
+
+```
+QRScannerView(codeHandler: { code in
+    // Handle the scanned code here
+})
+```
+The opacity, frameHeight, frameWidth, borderColor, borderWidth, borderHeight  parameters are optional, and control the opacity of the overlay, and the height and width of the camera frame. The codeHandler parameter is required, and should be a closure that takes a String parameter, which will contain the scanned QR code.
+
+The default values are opacity: 0.5, frameHeight: 250, frameWidth: 250, borderColor: .black, borderWidth: 5,borderHeight: 100.
+
+#Example
+Here's an example of how you can use the QRScannerView in your SwiftUI view:
+
+```
+struct ContentView: View {
+    @State private var isPresentingScanner = false
+    @State private var scannedCode = ""
+
+    var body: some View {
+        VStack {
+            Text("Scanned code: \(scannedCode)")
+            Button("Scan QR code") {
+                isPresentingScanner = true
+            }
+        }
+        .sheet(isPresented: $isPresentingScanner) {
+            QRScannerView { code in
+                scannedCode = code
+                isPresentingScanner = false
+            }
+        }
+    }
+}
+
+```
+
+#Support
+If you have any questions or issues with the QRScannerView package, you can contact me by opening an issue on the package repository: https://github.com/mrSolutionist/QRScannerViewKit .
--- Sources/QRScannerViewKit/QRScannerViewKit.swift
+++ Sources/QRScannerViewKit/QRScannerViewKit.swift
@@ -13,11 +13,20 @@ public struct QRScannerView: View {
     public var frameHeight: CGFloat
     public var frameWidth: CGFloat
     
-    public init(opacity: Double = 0.5, frameHeight: CGFloat = 250, frameWidth: CGFloat = 250,codeHandler: @escaping (String) -> Void) {
+    
+    public var borderWidth: CGFloat
+    public var borderColor : Color
+    
+    public var borderHeight: CGFloat
+    
+    public init(borderHeight:CGFloat = 100, borderColor: Color = .black,borderWidth:CGFloat = 10, opacity: Double = 0.5, frameHeight: CGFloat = 250, frameWidth: CGFloat = 250,codeHandler: @escaping (String) -> Void) {
         self.opacity = opacity
         self.frameHeight = frameHeight
         self.frameWidth = frameWidth
         self.codeHandler = codeHandler
+        self.borderWidth = borderWidth
+        self.borderColor = borderColor
+        self.borderHeight = borderHeight
     }
     // The handler that will be called with the scanned code
     public var codeHandler: (String) -> Void
@@ -34,23 +43,24 @@ public struct QRScannerView: View {
                 .edgesIgnoringSafeArea(.all)
                 
                 // Transparent black overlay
-                Color.black.opacity(0.5)
+                Color.black.opacity(opacity)
                     .edgesIgnoringSafeArea(.all)
                 
                 // Square portion to act as camera
                 RoundedRectangle(cornerRadius: 16)
-                    .frame(width: 250, height: 250)
+                    .frame(width: frameWidth, height: frameHeight)
                     .overlay(
                         RoundedRectangle(cornerRadius: 16)
                             .fill(Color.black.opacity(0.8))
-                            .frame(width: 250, height: 250)
+                            .frame(width: frameWidth, height: frameHeight)
                     )
                     .blendMode(.destinationOut)
                     .border(.white,width: 10)
                     .cornerRadius(12)
                 
-                QRScannerViewBorderDisplay()
-                    .blendMode(.destinationOut)
+                QRScannerViewBorderDisplay(borderColor: borderColor, borderWidth: borderWidth, frameHeight: frameHeight, frameWidth: frameWidth, borderHeight: borderHeight)
+//                .frame(width: 200, height: 100)
+                    
                 
                 Capsule()
                 
@@ -84,35 +94,47 @@ public struct QRScannerView: View {
 
 @available (iOS 16.0, *)
 public struct QRScannerViewBorderDisplay: View {
-    public init() {}
+    public var borderWidth: CGFloat
+    public var borderHeight: CGFloat
+    public var borderColor : Color
+    public var frameHeight: CGFloat
+    public var frameWidth: CGFloat
+    
+    public init(borderColor: Color,borderWidth: CGFloat ,frameHeight: CGFloat, frameWidth: CGFloat , borderHeight: CGFloat) {
+        self.borderWidth = borderWidth
+        self.frameHeight = frameHeight
+        self.frameWidth = frameWidth
+        self.borderColor = borderColor
+        self.borderHeight = borderHeight
+    }
     
     public var body: some View {
         Group {
             HStack {
                 VStack {
-                    Color.white
+                    borderColor
                 }
-                .frame(width: 10, height: 100)
+                .frame(width: borderWidth, height: borderHeight)
                 Spacer()
                 VStack {
-                    Color.white
+                    borderColor
                 }
-                .frame(width: 10, height: 100)
+                .frame(width: borderWidth, height: borderHeight)
             }
-            .frame(width: 250, height: 100)
+            .frame(width: frameWidth, height: frameHeight)
             
             VStack {
                 HStack {
-                    Color.white
+                    borderColor
                 }
-                .frame(width: 100, height: 10)
+                .frame(width: borderHeight, height: borderWidth)
                 Spacer()
                 HStack {
-                    Color.white
+                    borderColor
                 }
-                .frame(width: 100, height: 10)
+                .frame(width: borderHeight, height: borderWidth)
             }
-            .frame(width: 250, height: 250)
+            .frame(width: frameWidth, height: frameHeight)
         }
     }
 }
